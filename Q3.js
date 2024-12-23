9112632f5d6fb43fa074537ffd9bfe3782104fc1
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2024-12-23 17:00:24 +0530

    successfully pushed code from local

---
+++ Q3.js
@@ -0,0 +1,14 @@
+
+const a = document.getElementById("main-heading");
+a.innerHTML = "Fruit World";
+a.style.color = "orange";
+
+const b = document.getElementById("header");
+b.style.backgroundColor = "green";
+b.style.borderBottom = "2px solid orange";
+
+const c = document.getElementById("basket-heading");
+c.style.color = "green"
+
+const d = document.getElementById("thanks")
+d.innerHTML = "<p>Please visit us again</p>";
