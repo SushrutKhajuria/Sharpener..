44f6431dad39ef1f935bc8aa0f243fae16831bc7
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2024-12-24 13:05:25 +0530

    Q5 sol

--- sharpener JS sols.js
+++ sharpener JS sols.js
@@ -30,3 +30,23 @@ l[4].style.color = "blue";
 for (let i = 0; i < l.length; i++) { 
     l[i].style.fontStyle = "italic";
 }
+
+//Q5.  Write the code as shown in the video below:
+
+// Write answer to the questions asked below:
+const a = document.querySelector("#main-heading")
+a.style.textAlign = "right";
+
+const b = document.querySelector("#basket-heading")
+b.style.color = "brown"
+
+const c = document.querySelectorAll("li")
+
+for (let i = 0; i < c.length; i++) { 
+    if ((i+1) % 2 === 0) {
+        c[i].style.backgroundColor = "brown";
+        c[i].style.color = "white";
+    }
+   
+}
+
