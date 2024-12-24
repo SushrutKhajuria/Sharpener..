5e27a02c48220fcafe1cdc78578e89c606d72cc2
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2024-12-24 13:10:44 +0530

    Q7 Sol

--- sharpener JS sols.js
+++ sharpener JS sols.js
@@ -31,7 +31,7 @@ for (let i = 0; i < l.length; i++) {
     l[i].style.fontStyle = "italic";
 }
 
-//Q5.  Write the code as shown in the video below:
+//Q6.  Write the code as shown in the video below:
 
 // Write answer to the questions asked below:
 const a = document.querySelector("#main-heading")
@@ -50,3 +50,40 @@ for (let i = 0; i < c.length; i++) {
    
 }
 
+//Q7. 
+
+//html
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body style="margin: 0">
+    <div id="header" style="padding: 1rem 0; background-color: lightblue">
+      <h1 id="main-heading" style="margin: 0">Fruit Shop</h1>
+      <h3>Buy high quality organic fruits online</h3>
+    </div>
+    <div>
+      <h2 id="basket-heading">Fruits In Basket</h2>
+      <p id ="fruits-total">Total fruits: 4</p>
+      <ul class="fruits">
+        <li class="fruit">Banana</li>
+        <li class="fruit">Apple</li>
+        <li class="fruit">Orange</li>
+        <li class="fruit">Kiwi</li>
+      </ul>
+    </div>
+    <div id="thanks"></div>
+  </body>
+  <script src="index.js"></script>
+</html>
+
+
+//q7 javascript
+// Write your code below:
+var a = document.querySelector("h3")
+a.style.fontStyle = "italic";
