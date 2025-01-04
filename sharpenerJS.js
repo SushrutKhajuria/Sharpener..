aa15b49aae6215b9ea9ddc981b8acd9adc9bf2ed
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-01-04 21:30:03 +0530

    The wierd mystery , Q of Advanced JS

--- sharpenerJS.js
+++ sharpenerJS.js
@@ -1,69 +1,59 @@
-/* Q16 js */
-
-// Write your code below:
-function handleFormSubmit(event) {
-    event.preventDefault();
-
-    let username = event.target.username.value;
-    let email = event.target.email.value;
-    let phone = event.target.phone.value;
-
-    const newUser = {
-        username: username,
-        email: email,
-        phone: phone
-    };
-
-    const existingUsers = JSON.parse(localStorage.getItem("UserDetails")) || [];
-
-    existingUsers.push(newUser);
-
-    localStorage.setItem("UserDetails", JSON.stringify(existingUsers));
-
-    displayUsers(existingUsers);
-}
-
-function displayUsers(users) {
-    let userList = document.querySelector("ul");
-
-    userList.innerHTML = "";
-
-    users.forEach((user, index) => {
-        let listItem = document.createElement("li");
-        // Fixed string interpolation (enclosed in backticks)
-        listItem.innerHTML = `Username: ${user.username} Email: ${user.email} Phone: ${user.phone} <button onclick="deleteUser(${index})">Delete</button> <button onclick="editUser(${index})">Edit</button>`;
-
-        userList.appendChild(listItem);
-    });
-}
-
-function deleteUser(index) {
-    let existingUsers = JSON.parse(localStorage.getItem("UserDetails")) || [];
-
-    existingUsers.splice(index, 1);
-
-    localStorage.setItem("UserDetails", JSON.stringify(existingUsers));
-
-    displayUsers(existingUsers);
-}
-
-function editUser(index) {
-    let existingUsers = JSON.parse(localStorage.getItem("UserDetails")) || [];
-    let user = existingUsers[index];
-
-    let newUsername = prompt("Enter new username:", user.username);
-    let newEmail = prompt("Enter new email:", user.email);
-    let newPhone = prompt("Enter new phone number:", user.phone);
-
-    existingUsers[index] = {
-        username: newUsername,
-        email: newEmail,
-        phone: newPhone
-    };
-
-    localStorage.setItem("UserDetails", JSON.stringify(existingUsers));
-
-    displayUsers(existingUsers);
-}
-
-module.exports = handleFormSubmit;
+/* The wierd mystery
+ in Advanced javascript
+ */
+class Student {
+    constructor(name, age, marks) {
+      this.name = name;
+      this.age = age;
+      this.marks = marks;
+    }
+    setPlacementAge(minPlacementAge) {
+      return (minMarks) => {
+        if (this.marks >= minMarks && this.age >= minPlacementAge) {
+          return true;
+        }
+        else {
+          return false;
+        }
+      }
+  
+      //return a function which takes in argument -> minMarks
+      //returns true if students marks are gretaer than minMarks and age gretaer than minPlacementAge
+      //Complete this function only. Do not alter any other thing.
+    }
+  }
+  
+  
+  
+  //Do not touching anything below this line
+  
+  function createNewStudents(name, age, marks) {
+    const Riya = new Student(name, age, marks);
+  
+    console.log(Riya.setPlacementAge(18)(40))
+  
+  }
+  
+  async function readInput() {
+  
+    let inputString = '';
+  
+    var output = [];
+  
+    process.stdin.on('data', inputStdin => {
+  
+      inputString += inputStdin;
+  
+      const inputArr = inputString.split(/(?:\r\n|\r|\n)/g)
+  
+      const argumentsArr = inputArr[0].split(',');
+  
+      createNewStudents(argumentsArr[0], Number(argumentsArr[1]), Number(argumentsArr[2]))
+  
+      process.exit();
+  
+    })
+  
+  }
+  
+  readInput();
